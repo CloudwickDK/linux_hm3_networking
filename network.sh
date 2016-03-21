c37bbb719193fd6f6c5152f9a1cf9df45b986937
Despoina Karna <falsedk@hotmail.com>
2016-03-21 11:54:00 +0000

    initial  commit

---
+++ network.sh
@@ -0,0 +1,16 @@
+#!\bin\bash
+
+# 1) In order to install telnet run:
+#sudo yum install telnet
+
+# 2) run wireshark with command: 
+sudo tshark
+
+# 3) cntrl shift new to open another terminal 
+# 4) run telnet to request google.com (port 80)
+telnet google.com 80
+
+# 5) Tyoe GET at the second terminal
+
+
+#
